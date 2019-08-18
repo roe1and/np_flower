724051ef0bdbc8e9e512b95dbcdc346939016edf
Roeland Schumacher <schumies@gmx.co.uk>
2019-08-18 21:19:03 +0100

    testing

--- generate_code.py
+++ generate_code.py
@@ -15,9 +15,34 @@ for x in range(im.size[0]):
         #print(im.size[1])
         pixels.append(pic[x, y])
         image_rgb_values.append(pic[x, y])
-print(image_rgb_values)
-print(len(image_rgb_values))
-chunk_list = [image_rgb_values[i:i+16] for i in range(0, len(image_rgb_values), 16)]
+chunk_list = [image_rgb_values[i:i+4] for i in range(0, len(image_rgb_values), 4)]
+image_rgb_values_copy1 = image_rgb_values.copy()
+
+image_rgb_values1 = []
+o = image_rgb_values_copy1.pop(0)
+
+for v in image_rgb_values_copy1:
+    image_rgb_values1.append(v)
+image_rgb_values1.append(o)
+
+#chunk_list = [image_rgb_values[i:i+4] for i in range(0, len(image_rgb_values), 4)]
+#print(chunk_list)
+#for i, rgb in enumerate(chunk_list):
+#    print(rgb)
+
+def move_over(l):
+    return_list_copy = l.copy()
+    return_list = []
+    first = return_list_copy.pop()
+    return_list.append(first)
+    return_list.append(return_list_copy)
+    return return_list
+
+for i in range(im.size[0]):
+    print(move_over(chunk_list))
+
+
+
 with open('out.txt', 'w') as outfile:
     for i, rgb in enumerate(chunk_list):
         for j, pixel in enumerate(rgb):
