292540484c297ed8d82f1c15355d93f3425f78e0
Roeland Schumacher <schumies@gmx.co.uk>
2019-08-23 22:11:12 +0100

    testing

--- generate_code.py
+++ generate_code.py
@@ -4,9 +4,9 @@ from PIL import Image
 image = 'test.jpg'
 im = Image.open(image)
 pic = im.load()
-print(range(im.size[1]))
+# print(range(im.size[1]))
 image_rgb_values = []
-print(im.size[0], im.size[1])
+# print(im.size[0], im.size[1])
 
 for x in range(im.size[0]):
     pixels = []
@@ -34,15 +34,47 @@ def move_over(l):
     return_list_copy = l.copy()
     return_list = []
     first = return_list_copy.pop()
-    return_list.append(first)
     return_list.append(return_list_copy)
+    return_list.append(first)
     return return_list
 
-for i in range(im.size[0]):
-    print(move_over(chunk_list))
+outfile = open('out.txt', 'w')
 
+def output(num, inp):
+    # print(inp)
+    led = 0
+    print(num, 'dammit')
+    temp = []
+    working = inp.copy()
+    first = working.pop(0)
+    # print(first)
+    for a in working:
+        temp.append(a)
+    temp.append(first)
+    for frame in working:
+        # print(frame)
+        for i in frame:
+            line = 'setPixelColor({}, {});'.format(led, i)
+            print(line)
+            led += 1
+    for j in first:        
+        line = 'setPixelColor({});'.format(i)
+        print(line)
+    print('delay(100)')
+    num -= 1
 
+    if num == 14:
+        return 1
+    else:
+        output(num, temp)
+        #for f in frame:
+        #    print(f)
+    # print(temp)
 
+print('before', chunk_list)
+output(im.size[0], chunk_list)
+
+'''
 with open('out.txt', 'w') as outfile:
     for i, rgb in enumerate(chunk_list):
         for j, pixel in enumerate(rgb):
@@ -53,3 +85,4 @@ with open('out.txt', 'w') as outfile:
             outfile.write("\n")
         outfile.write('strip.show();')
         outfile.write("\n")
+'''
