63746dc145d82c52c054e305748e7daa802d4dc9
Roeland Schumacher <schumies@gmx.co.uk>
2019-08-24 20:44:33 +0100

    nasty but it works

--- generate_code.py
+++ generate_code.py
@@ -39,7 +39,7 @@ def move_over(l):
     return return_list
 
 outfile = open('out.txt', 'w')
-
+'''
 def output(num, inp):
     # print(inp)
     led = 0
@@ -70,8 +70,130 @@ def output(num, inp):
         #for f in frame:
         #    print(f)
     # print(temp)
+'''
 
-print('before', chunk_list)
+def output(num, inp):
+    # print(inp)
+    led = 0
+    # print(num, 'dammit')
+    temp = []
+    working = inp.copy()
+    first = working.pop(0)
+    # print(first)
+    for a in working:
+        temp.append(a)
+    temp.append(first)
+    # print(temp)
+
+    '''
+    for frame in working:
+        # print(frame)
+        for i in frame:
+            line = 'setPixelColor({}, {});'.format(led, i)
+            print(line)
+            led += 1
+    for j in first:        
+        line = 'setPixelColor({});'.format(i)
+        print(line)
+    print('delay(100)')
+    '''
+    num -= 1
+
+    if num == 0:
+        back =  {'a': 0, 'b': 4, 'c': 8, 'd': 12, 'num': 0}
+        l3 =    {'a': 1, 'b': 5, 'c': 9, 'd': 13, 'num': 1}
+        l2 =    {'a': 2, 'b': 6, 'c': 10, 'd': 14, 'num': 2}
+        front = {'a': 3, 'b': 7, 'c': 11, 'd': 15, 'num': 3}
+
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(back['a'], temp[back['a']][back['num']][0], temp[back['a']][back['num']][1], temp[back['a']][back['num']][2])
+        print(line)
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(back['b'], temp[back['b']][back['num']][0], temp[back['b']][back['num']][1], temp[back['b']][back['num']][2])
+        print(line)
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(back['c'], temp[back['c']][back['num']][0], temp[back['c']][back['num']][1], temp[back['c']][back['num']][2])
+        print(line)
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(back['d'], temp[back['d']][back['num']][0], temp[back['d']][back['num']][1], temp[back['d']][back['num']][2])
+        print(line)
+        
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(l2['a'], temp[l2['a']][l2['num']][0], temp[l2['a']][l2['num']][1], temp[l2['a']][l2['num']][2])
+        print(line)
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(l2['b'], temp[l2['b']][l2['num']][0], temp[l2['b']][l2['num']][1], temp[l2['b']][l2['num']][2])
+        print(line)
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(l2['c'], temp[l2['c']][l2['num']][0], temp[l2['c']][l2['num']][1], temp[l2['c']][l2['num']][2])
+        print(line)
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(l2['d'], temp[l2['d']][l2['num']][0], temp[l2['d']][l2['num']][1], temp[l2['d']][l2['num']][2])
+        print(line)
+
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(l3['a'], temp[l3['a']][l3['num']][0], temp[l3['a']][l3['num']][1], temp[l3['a']][l3['num']][2])
+        print(line)
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(l3['b'], temp[l3['b']][l3['num']][0], temp[l3['b']][l3['num']][1], temp[l3['b']][l3['num']][2])
+        print(line)
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(l3['c'], temp[l3['c']][l3['num']][0], temp[l3['c']][l3['num']][1], temp[l3['c']][l3['num']][2])
+        print(line)
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(l3['d'], temp[l3['d']][l3['num']][0], temp[l3['d']][l3['num']][1], temp[l3['d']][l3['num']][2])
+        print(line)
+
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(front['a'], temp[front['a']][front['num']][0], temp[front['a']][front['num']][1], temp[front['a']][front['num']][2])
+        print(line)
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(front['b'], temp[front['b']][front['num']][0], temp[front['b']][front['num']][1], temp[front['b']][front['num']][2])
+        print(line)
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(front['c'], temp[front['c']][front['num']][0], temp[front['c']][front['num']][1], temp[front['c']][front['num']][2])
+        print(line)
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(front['d'], temp[front['d']][front['num']][0], temp[front['d']][front['num']][1], temp[front['d']][front['num']][2])
+        print(line)
+        
+        print('strip.show();')
+        print('delay(111);')
+        return 1
+    else:
+        back =  {'a': 0, 'b': 4, 'c': 8, 'd': 12, 'num': 0}
+        l3 =    {'a': 1, 'b': 5, 'c': 9, 'd': 13, 'num': 1}
+        l2 =    {'a': 2, 'b': 6, 'c': 10, 'd': 14, 'num': 2}
+        front = {'a': 3, 'b': 7, 'c': 11, 'd': 15, 'num': 3}
+
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(back['a'], temp[back['a']][back['num']][0], temp[back['a']][back['num']][1], temp[back['a']][back['num']][2])
+        print(line)
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(back['b'], temp[back['b']][back['num']][0], temp[back['b']][back['num']][1], temp[back['b']][back['num']][2])
+        print(line)
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(back['c'], temp[back['c']][back['num']][0], temp[back['c']][back['num']][1], temp[back['c']][back['num']][2])
+        print(line)
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(back['d'], temp[back['d']][back['num']][0], temp[back['d']][back['num']][1], temp[back['d']][back['num']][2])
+        print(line)
+        
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(l2['a'], temp[l2['a']][l2['num']][0], temp[l2['a']][l2['num']][1], temp[l2['a']][l2['num']][2])
+        print(line)
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(l2['b'], temp[l2['b']][l2['num']][0], temp[l2['b']][l2['num']][1], temp[l2['b']][l2['num']][2])
+        print(line)
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(l2['c'], temp[l2['c']][l2['num']][0], temp[l2['c']][l2['num']][1], temp[l2['c']][l2['num']][2])
+        print(line)
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(l2['d'], temp[l2['d']][l2['num']][0], temp[l2['d']][l2['num']][1], temp[l2['d']][l2['num']][2])
+        print(line)
+
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(l3['a'], temp[l3['a']][l3['num']][0], temp[l3['a']][l3['num']][1], temp[l3['a']][l3['num']][2])
+        print(line)
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(l3['b'], temp[l3['b']][l3['num']][0], temp[l3['b']][l3['num']][1], temp[l3['b']][l3['num']][2])
+        print(line)
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(l3['c'], temp[l3['c']][l3['num']][0], temp[l3['c']][l3['num']][1], temp[l3['c']][l3['num']][2])
+        print(line)
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(l3['d'], temp[l3['d']][l3['num']][0], temp[l3['d']][l3['num']][1], temp[l3['d']][l3['num']][2])
+        print(line)
+
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(front['a'], temp[front['a']][front['num']][0], temp[front['a']][front['num']][1], temp[front['a']][front['num']][2])
+        print(line)
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(front['b'], temp[front['b']][front['num']][0], temp[front['b']][front['num']][1], temp[front['b']][front['num']][2])
+        print(line)
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(front['c'], temp[front['c']][front['num']][0], temp[front['c']][front['num']][1], temp[front['c']][front['num']][2])
+        print(line)
+        line = 'strip.setPixelColor({}, {}, {}, {});'.format(front['d'], temp[front['d']][front['num']][0], temp[front['d']][front['num']][1], temp[front['d']][front['num']][2])
+        print(line)
+
+        print('strip.show();')
+        print('delay(111);')
+        #for f in temp:
+        #    print(f)
+        output(num, temp)
+    # print(temp)
+
+# print('before', chunk_list)
 output(im.size[0], chunk_list)
 
 '''
